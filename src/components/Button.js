5ae450821ddf788c812503ff03d717804950d6fb
Nikita Tarasov <nikita.tarasov@wynd.eu>
2019-12-11 00:27:29 +0300

    Refactor some style shit, and do amazing buttons with super realistic logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,10 @@
-import React from 'react'; 
+import React from 'react';
 export function Button(props) {
+    const style = props.isNext ? "next-button" : "prev-button"; //тернарный оператор 
     return (
-        <button disabled={this.props.disabled}>{props.value}</button>
+        <div onClick={props.onClick}>
+            {!props.isNext && <img src="/images/arrow.png" />}
+            <button className={style} disabled={props.disabled} >{props.children}</button>
+        </div>
     )
 }
